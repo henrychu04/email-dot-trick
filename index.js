59c35b344ee59ec99f2fc1ce75a6a444c819a103
henrychu04 <henrychu04@outlook.com>
2020-05-04 16:04:39 -0400

    edits

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const fs = require('fs')
 
 function append(fileName, jigged) {
     fs.appendFileSync(fileName, jigged, function (err) {
-        if (err) throw err
+        if (err) throw err;
     })
 }
 
 async function main() {
     try {
-        let email
+        let email;
  
         try {
-            email = await emailPrompt()
+            email = await emailPrompt();
         } catch (err) {
-            console.log('\n> Aborted!')
-            return
+            console.log('\n> Aborted!');
+            return;
         }
     
-        var text = email.split('@')[0]
+        var text = email.split('@')[0];
 
         fs.open(`${text}.txt`, 'w', function (err) {
             if (err) throw err;
@@ -26,37 +26,37 @@ async function main() {
     
         for (i = 0; i < text.length; i++) {
             if (i == 0) {
-                continue
+                continue;
             } else {
-                var firstHalf = text.substring(0, i)
-                var secondHalf = text.substring(i)
-                var jigged = `${firstHalf}.${secondHalf}@gmail.com\n`
+                var firstHalf = text.substring(0, i);
+                var secondHalf = text.substring(i);
+                var jigged = `${firstHalf}.${secondHalf}@gmail.com\n`;
 
-                append(`${text}.txt`, jigged)
+                append(`${text}.txt`, jigged);
 
-                var secondModified = secondHalf
+                var secondModified = secondHalf;
 
                 for (j = 0; j < secondModified.length; j++) {
                     if (j == 0) {
-                        continue
+                        continue;
                     } else {
-                        var first = secondModified.substring(0, j)
-                        var second = secondModified.substring(j)
-                        secondModified = first + '.' + second
+                        var first = secondModified.substring(0, j);
+                        var second = secondModified.substring(j);
+                        secondModified = first + '.' + second;
 
-                        var secondJigged = `${firstHalf}.${secondModified}@gmail.com\n`
+                        var secondJigged = `${firstHalf}.${secondModified}@gmail.com\n`;
 
-                        append(`${text}.txt`, secondJigged)
-                        j++
+                        append(`${text}.txt`, secondJigged);
+                        j++;
                     }
                 }                
             }
         }
 
-        console.log(`\n> Emails appended to ${text}.txt`)
+        console.log(`\n> Emails appended to ${text}.txt`);
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }  
 }
 
-main()
+main();
